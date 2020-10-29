c62cbfac749fe162e2425d8449705367f5fc53a6
Prasad Hewage <prasad.h@simulation.com.sg>
2020-10-29 20:56:45 +0800

    init project

---
+++ src/components/ProgressBoard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Hi I am the main board</div>
+</template>
+
+<script>
+export default {
+  name: "progress-board",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+</style>
